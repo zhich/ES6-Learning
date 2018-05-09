4d7eb8cfe3964819d7015f16354b6f23da6f1760
zch <1292984549@qq.com>
2018-05-09 23:04:18 +0800

    init

---
+++ getting-started/dist/js/02-let-const.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * var : 声明全局变量
+ */
+var name = "zch";
+window.onload = function () {
+  console.log(name);
+};
+
+/**
+ * let : 声明局部变量
+ */
+{
+  var _name = "aaa";
+}
+
+console.log(name); // name 还是 "zch"
+
+for (var i = 0; i < 10; i++) {
+  console.log('循环体中：' + i);
+}
+
+// console.log('循环体外：' + i); // 控制台报错
+
+/**
+ * const : 声明常量
+ */
+
+var x = 1;
+// x = 2; // 编译报错
+// var x = 3; // 编译报错
+console.log(x);

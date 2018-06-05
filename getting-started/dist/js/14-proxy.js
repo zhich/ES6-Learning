9ad01ad5629b9675e7e7202d16829292f431a7ae
zch <1292984549@qq.com>
2018-06-05 22:04:49 +0800

    map 数据结构 && 用 Proxy 进行预处理

---
+++ getting-started/dist/js/14-proxy.js
@@ -0,0 +1,104 @@
+'use strict';
+
+/**
+ * 14 用 Proxy 进行预处理
+ * Proxy 的存在可以让我们给函数加上类似钩子函数的代码，也可以理解为在执行方法前预处理一些代码。可以简单的理解为它是函数或者对象的生命周期
+ */
+
+/**
+ * 一、先来回顾一下定义对象的方法
+ */
+var obj = {
+    add: function add(a, b) {
+        return a + b;
+    },
+    name: 'zch'
+};
+console.log(obj.add(1, 2)); // 3
+console.log(obj.name); // zch
+
+/**
+ * 二、声明 Proxy
+ */
+/**
+ * 1、声明 Proxy 的基本形式
+ * 需要注意的是这里是两个花括号，第一个花括号就相当于我们方法的主体，
+ * 后边的花括号就是 Proxy 代理处理区域，相当于我们写钩子函数的地方。
+ */
+new Proxy({}, {});
+
+/**
+ * 2、把上边的 obj 对象改成我们的 Proxy 形式
+ */
+var pro = new Proxy({
+    add: function add(a, b) {
+        return a + b;
+    },
+    name: 'zch'
+}, {
+    get: function get(target, key, property) {
+        console.log('come in get');
+        return target[key];
+    }
+});
+
+// 可以在控制台看到结果，先输出了 come in get , 相当于在方法调用前的钩子函数
+console.log(pro.add(1, 7)); // come in get  -->  8
+console.log(pro.name); // come in get  -->  zch
+
+/**
+ * 3、get 属性
+ * get 属性是在你得到某对象属性值时预处理的方法，他接受三个参数
+ *     target：得到的目标值
+ *     key：目标的 key 值，相当于对象的属性
+ *     property：这个不太常用，用法还在研究中，还请大神指教。
+ */
+
+/**
+ * 4、set 属性
+ * set 属性是指你要改变 Proxy 属性值时，进行的预先处理。它接收四个参数。
+ *     target：目标值。
+ *     key：目标的 key 值。
+ *     value：要改变的值。
+ *     receiver：改变前的原始值。
+ */
+
+var myPro = new Proxy({
+    add: function add(a, b) {
+        return a + b;
+    },
+    name: 'zch'
+}, {
+    get: function get(target, key) {
+        console.log('come in get');
+        return target[key];
+    },
+    set: function set(target, key, value, receiver) {
+        console.log('setting ' + key + ' = ' + value);
+        return target[key] = value;
+    }
+});
+
+console.log('---------------------');
+
+console.log(myPro.name); // come in get  -->  zch
+myPro.name = '明年今日'; // setting name = 明年今日
+console.log(myPro.name); // come in get  -->  明年今日
+
+/**
+ * 三、apply 的使用
+ * apply 的作用是调用内部的方法，它使用在方法体是一个匿名函数时。看下边的代码。
+ */
+var target = function target() {
+    return 'zch';
+};
+var handler = {
+    apply: function apply(target, ctx, args) {
+        console.log('do apply');
+        return Reflect.apply.apply(Reflect, arguments);
+    }
+};
+
+console.log('---------------------');
+var myPro1 = new Proxy(target, handler);
+console.log(myPro1()); // do apply  -->  zch
---
+++ getting-started/src/js/13-map.js
@@ -0,0 +1,69 @@
+/**
+ * 13 map 数据结构
+ */
+
+/**
+ * 一、Json 和 map 格式的对比
+ * map 的效率和灵活性更好
+ */
+
+/**
+ * 先来写一个 JSON，这里我们用对象进行模拟操作。
+ */
+let json = {
+    name: 'zch',
+    skill: 'Android'
+};
+console.log(json.name);
+
+/**
+ * 但是这种反应的速度要低于数组和 map 结构。而且 map 的灵活性要更好，你可以把它看成一种特殊的键值对，
+ * 但你的 key 可以设置成数组，值也可以设置成字符串，让它不规律对应起来。
+ */
+var map = new Map();
+map.set(json, 'abc');
+console.log(map);
+console.log(map.get(json)); // abc
+
+/**
+ * 当然也可 key 是字符串，value 是对象。
+ */
+map.set('name', json);
+console.log(map);
+console.log(map.get('name')); // {name: "zch", skill: "Android"}
+
+/**
+ * 二、map 的增删查
+ */
+/**
+ * 1、设值 & 取值
+ */
+map.set('key', 123);
+console.log(map.get('key')); // 123
+
+/**
+ * 2、删除 delete
+ */
+map.delete('key');
+map.delete(json);
+console.log(map);
+
+/**
+ * 3、size 属性
+ */
+console.log(map.size); // 1
+
+/**
+ * 4、查找是否存在 has
+ */
+console.log(map.has(json)); // false
+console.log(map.has('name')); // true
+console.log(map.has('key')); // false
+
+/**
+ * 5、清除所有元素 clear
+ */
+map.clear();
+console.log(map); // { }
+
+// 总结：map 在现在开发中已经经常使用，它的灵活性和高效性是我们喜欢的
